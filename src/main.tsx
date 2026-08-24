7524cb67079210da12d7eaa5cbe2f0d323dcea2c
otug-agro <augustof.agro@gmail.com>
2026-08-24 14:38:51 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import AuthGate from "./AuthGate";
 import "./styles.css";
 
 const root = document.getElementById("root");
@@ -11,6 +11,6 @@ if (!root) {
 
 createRoot(root).render(
   <React.StrictMode>
-    <App />
+    <AuthGate />
   </React.StrictMode>,
 );
